2411a239c43e0d97ccb3f442cece7ad6f619dd7c
calaf <117057567+ca1af@users.noreply.github.com>
2024-12-01 11:50:37 +0900

    feat : 완료

--- src/main/kotlin/calculator/CalculatorController.kt
+++ src/main/kotlin/calculator/CalculatorController.kt
@@ -1,12 +1,16 @@
 package calculator
 
 import calculator.delimiter.DelimiterFactory
+import camp.nextstep.edu.missionutils.Console
 
 class CalculatorController {
-    fun calculate(input: String): Int {
+    fun calculate() {
+        println("덧셈할 문자열을 입력해 주세요.")
+        val input = Console.readLine()
         val calculator = Calculator()
         val delimiterFactory = DelimiterFactory()
         val delimiter = delimiterFactory.getInstance(input)
-        return calculator.calculate(input, delimiter)
+        val result = calculator.calculate(input, delimiter)
+        println("결과 : $result")
     }
 }
